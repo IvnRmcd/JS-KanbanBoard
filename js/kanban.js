5f1e1fb6015268ef06816b5efd7fc27205b8cfbe
Ivan <ivn.cox@outlook.com>
2022-03-27 14:57:09 -0400

    Add save button

--- js/kanban.js
+++ js/kanban.js
@@ -27,12 +27,15 @@ const create_item = () => {
 
   item.addEventListener('dragend', (event) => {
     event.dataTransfer.clearData();
-
-
-    let input = document.createElement('input');
-    item.appendChild(input)
-
   });
+
+
+  let input = document.createElement('input');
+  item.appendChild(input)
+
+
+  let save_btn = document.createElement('button');
+  save_btn.innerHTML('Save')
 };
 
 document.querySelectorAll('.drop').forEach(element => {
